d9953f68b559de800103a722cfebc70ce5a1c6e7
charlie-daniels <charlied030401@gmail.com>
2022-09-16 14:33:53 +0100

    feat: Add helper selector module

---
+++ src/modules/domcollection.js
@@ -0,0 +1,18 @@
+// Navbar
+export const navMenu = document.getElementById('show-menu');
+export const navHome = document.getElementById('show-home');
+export const navSearch = document.getElementById('search-query');
+export const navNewItem = document.getElementById('new-item');
+export const navNotifications = document.getElementById('show-notifications');
+// Sidebar
+export const sideGeneral = document.getElementById('general');
+export const sideFavourites = document.getElementById('favourites');
+export const sideProjects = document.getElementById('projects');
+export const sideGeneralList = document.querySelector('#general > ul');
+export const sideFavouritesList = document.querySelector('#favourites > ul');
+export const sideProjectsList = document.querySelector('#projects > ul');
+// Dashboard
+export const dashboardTitle = document.getElementById('storage-title');
+export const dashboardFilters = document.getElementById('filters');
+export const dashboardItemList = document.getElementById('item-list');
+export const dashboardFirstItem = document.querySelector('dash-item');
